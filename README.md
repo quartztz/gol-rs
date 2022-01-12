d356df7463feb5bf7d38ee60153970687a2b6d0e
quartztz <jakimoretti@gmail.com>
2022-01-12 15:21:57 +0100

    added State management, pausing/unpausing

--- README.md
+++ README.md
@@ -12,11 +12,16 @@ cargo build --release && cargo run --release
 
 This has only been tested on linux: the ncurses library that was used ([this  one](https://github.com/ihalila/pancurses)) is supposedly cross platform but this hasn't been tested.
 
+## "Controls"
+
+Upon launch, a blank window is shown: the game can be started by using the `s` key on your keyboard. Playing will start instantly, and its paused state can be toggled using the `Enter` (or `Return`) key. Quit with the `q` key.
+
 ## To Do
 
 I still want to add a couple of things, so i can't say it's done just yet. Namely: 
 
- - [ ] State management: add a `State` enumeration to allow for setup/pausing/a lot of fun stuff. 
- - [ ] Better printing and refreshing: works fine in my hyper terminal window but the default Chrome OS terminal has very noticeable and hideous "scanlines" so I'll see what I can do about that
+ - [x] State management: add a `State` enumeration to allow for setup/pausing/a lot of fun stuff. 
+ - [ ] Since state management took way less than I was anticipating, maybe add the ability to set the initial state of the board by using arrow keys to toggle/maybe mouse integration. this should keep me busy for a little longer.
+ - [ ] Better printing and refreshing: works fine in my hyper terminal window but the default Chrome OS terminal has very noticeable and hideous "scanlines" so I'll see what I can do about that.
  - [ ] If anyone has suggestions/issues, report them and I'll be glad to help!
 
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,65 @@ use pancurses::*;
 use std::time::Duration;
 use std::thread::sleep;
 
+static SLEEP_TIME: Duration = Duration::from_millis(300);
+
+enum State {
+	INIT(),
+	SIMULATION(),
+	PAUSED(),
+	END(),
+}
+
+fn update_simulation(win: &Window, grid: &mut Grid) -> State {
+	win.clear();
+	grid.update();
+	grid.draw(win);
+
+	sleep(SLEEP_TIME);
+
+	match win.getch() {
+		Some(Input::Character(c)) => { 
+			if c == 'q' { 
+				State::END()
+			} else if c == '\n' {
+				State::PAUSED()
+			}else {
+				State::SIMULATION()
+			}},
+		Some(_input) => { State::SIMULATION() },
+		None => ( State::SIMULATION() ),
+	}
+}
+
+fn init(win: &Window) -> State {
+	// TODO: state setup using mouse before starting the simulation.
+	match win.getch() {
+		Some(Input::Character(c)) => {
+			if c == 's' {
+				State::SIMULATION()
+			} else {
+				State::INIT() 
+			}
+		}
+		Some(_input) => State::INIT(),
+		None => State::INIT(),
+	}
+}
+
+fn paused(win: &Window) -> State {
+	match win.getch() {
+		Some(Input::Character(c)) => {
+			if c == '\n' {
+				State::SIMULATION()
+			} else {
+				State::PAUSED()
+			}
+		}
+		Some(_input) => State::PAUSED(),
+		None => State::PAUSED()
+	}
+}
+
 fn main() {
 	let main_win = initscr(); 
 	curs_set(0);
@@ -14,22 +73,17 @@ fn main() {
 	main_win.nodelay(true);
 	let (max_y, max_x) = main_win.get_max_yx();
 
-	let sleep_time = Duration::from_millis(300);
-
 	let mut grid = build_grid_from_scratch(max_y, max_x);
+	let mut current_state: State = State::INIT();
 
 	'main: loop {
-		main_win.clear();
-		grid.update();
-		grid.draw(&main_win);
-	
-		sleep(sleep_time);
-
-		match main_win.getch() {
-			Some(Input::Character(c)) => { if c == 'q' { break 'main; } },
-			Some(_input) => {},
-			None => (),
-		} 
+		match current_state {
+			State::INIT() => { current_state = init(&main_win) },
+			State::SIMULATION() => { current_state = update_simulation(&main_win, &mut grid) }
+			State::PAUSED() => { current_state = paused(&main_win) }
+			State::END() => { break 'main; }
+		}
 	}
+
 	endwin();
 }
